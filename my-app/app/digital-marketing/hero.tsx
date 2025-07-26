07f360e61870dd1077a07d92867a0a5855ba9bbf
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2025-07-26 12:56:22 +0100

    Added jean texture

--- my-app/app/digital-marketing/hero.tsx
+++ my-app/app/digital-marketing/hero.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+import jeanImage from "../../public/preview.jpg";
+
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Digital Marketing Hero</h1>
-      <p className="text-lg text-gray-700">Empowering your brand with innovative strategies.</p>
+    <div className="flex flex-row items-center justify-center h-[80vh] bg-gray-100">
+      <div className="w-1/2 h-full">
+        <Image 
+          priority
+          src={jeanImage}
+          alt="Jean texture"
+          className="w-full h-full object-cover"
+          width={1024}
+          height={768}
+          placeholder="empty"
+          unoptimized
+        />
+      </div>
+      <div className="w-1/2"></div>
     </div>
   );
 }
