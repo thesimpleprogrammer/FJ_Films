d4ae675d212a42073f0175969ba66306c75b781b
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2025-07-29 20:04:37 +0100

    Changed file name servicesContainer to temp

--- my-app/app/digital-marketing/page.tsx
+++ my-app/app/digital-marketing/page.tsx
@@ -2,7 +2,7 @@ import HeroContainer from "./HeroContainer";
 import Clients from "./clients";
 import AboutContainer from "./AboutContainer";
 import AboutExtrasContainer from "./AboutExtrasContainer";
-import ServicesContainer from "./servicesContainer";
+import ServicesContainer from "./Temp";
 
 export default function Page() {
   return (
