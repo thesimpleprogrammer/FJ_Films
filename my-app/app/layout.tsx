2acbb456d5f2818acdc11dbcf718f1b536bf2fd3
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2025-11-17 08:09:15 +0100

    Added google adsense

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -24,7 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9187514096050957"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
