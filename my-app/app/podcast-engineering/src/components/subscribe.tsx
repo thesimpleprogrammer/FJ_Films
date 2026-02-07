1bff831575d1c36356f7f558999fdf2ada5f4c79
Ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2026-02-08 00:21:43 +0100

    Changed the podcast subscription buttons

--- my-app/app/podcast-engineering/src/components/subscribe.tsx
+++ my-app/app/podcast-engineering/src/components/subscribe.tsx
@@ -30,7 +30,7 @@ export default function Subscribe({ podcastData, userData }: any) {
         >
           {getContent("subscribe_h1")}
         </ReusableEditableText>
-        <div className="flex flex-col md:w-[60%] md:flex-row gap-3">
+        <div className="flex flex-col md:w-[35%] md:flex-row gap-3">
           <button onClick={() => router.push("https://www.youtube.com/@FREETALKPODCASTLIVE")} className="flex items-center justify-between group border border-white hover:bg-white hover:text-black px-4 py-2 transition-colors duration-300 hover:cursor-pointer">
             Watch on Youtube
             <svg className="ml-3 fill-white transition-colors duration-300 group-hover:fill-black group-hover:stroke-white w-[10%] md:w-[15%] inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M581.7 188.1C575.5 164.4 556.9 145.8 533.4 139.5C490.9 128 320.1 128 320.1 128C320.1 128 149.3 128 106.7 139.5C83.2 145.8 64.7 164.4 58.4 188.1C47 231 47 320.4 47 320.4C47 320.4 47 409.8 58.4 452.7C64.7 476.3 83.2 494.2 106.7 500.5C149.3 512 320.1 512 320.1 512C320.1 512 490.9 512 533.5 500.5C557 494.2 575.5 476.3 581.8 452.7C593.2 409.8 593.2 320.4 593.2 320.4C593.2 320.4 593.2 231 581.8 188.1zM264.2 401.6L264.2 239.2L406.9 320.4L264.2 401.6z"/></svg>
